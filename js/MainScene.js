5a59b5727f08074b1fa2b1f0d3b264ba76ac644a
KonradPietrzak <kondzio1333@o2.pl>
2023-12-09 10:51:26 +0000

    Load Tilemap
    1:00:30 - failed

--- js/MainScene.js
+++ js/MainScene.js
@@ -6,6 +6,8 @@ export default class MainScene extends Phaser.Scene {
 
   preload() {
     Player.preload(this);
+    this.load.image('tiles', 'assets/images/RPG Nature Tileset.png');
+    this.load.tilemapTiledJSON('map', 'assets/images/map.json');
   }
 
   create() {
@@ -17,6 +19,11 @@ export default class MainScene extends Phaser.Scene {
       texture:'female',
       frame:'townfolk_f_idle_1'
   });
+  
+const map = this.make.tilemap({key:'map'});
+const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
+const layer1 = map.createStaticLayer('Tile Layer 1', tileset,0 ,0);
+
     this.player.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
