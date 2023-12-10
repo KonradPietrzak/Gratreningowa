cf3a922ee77d6909ea37b59df043f6f72a7460f0
KonradPietrzak <kondzio1333@o2.pl>
2023-12-10 14:13:14 +0000

    Collides
    Colliders added into map,  response with char's circle collider, finito '_'

--- js/MainScene.js
+++ js/MainScene.js
@@ -12,19 +12,22 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     console.log("create");
-    this.player = new Player({
-      scene:this,
-      x:0,
-      y:0,
-      texture:'female',
-      frame:'townfolk_f_idle_1'
-  });
+    
   
-const map = this.make.tilemap({key:'map'});
-const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
-const layer1 = map.createStaticLayer('Tile Layer 1', tileset,0 ,0);
-
-    this.player.inputKeys = this.input.keyboard.addKeys({
+  const map = this.make.tilemap({key:'map'});
+  const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
+  const layer1 = map.createLayer('Tile Layer 1', tileset,0 ,0);
+  const layer2 = map.createLayer('Tile Layer 2', tileset,0 ,0);
+  layer1.setCollisionByProperty({collides:true})
+  this.matter.world.convertTilemapLayer(layer1)
+  this.player = new Player({
+    scene:this,
+    x:40,
+    y:40,
+    texture:'female',
+    frame:'townfolk_f_idle_1'
+});
+  this.player.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
       left: Phaser.Input.Keyboard.KeyCodes.A,
