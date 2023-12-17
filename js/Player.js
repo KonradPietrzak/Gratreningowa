25ed767ba7a71b40e8b0f8552cc4263cb32c74a4
KonradPietrzak <kondzio1333@o2.pl>
2023-12-17 09:37:28 +0000

    Surival Game
    17. Loaded Pickaxe spirte,, attached to the char,  Looped pickaxe movement at a specific angle, flip the sprite when the cursor moves to the left or right for the char or pickaxe.

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,11 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         let{scene,x,y,texture,frame} = data;
         super(scene.matter.world,x,y,texture,frame);
         this.scene.add.existing(this);
+        //Weapon
+        this.spriteWeapon = new Phaser.GameObjects.Sprite(this.scene,0,0,'items',162);
+        this.spriteWeapon.setOrigin(0.25,0.75);
+        this.scene.add.existing(this.spriteWeapon);
+   
         
         const {Body, Bodies} = Phaser.Physics.Matter.Matter;
         var playerCollider = Bodies.circle(this.x, this.y,12, {isSensor:false, label:'playerCollider'});
@@ -13,12 +18,15 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         });
         this.setExistingBody(compoundBody);
         this.setFixedRotation();
+
+        this.scene.input.on('pointermove', pointer => this.setFlipX(pointer.worldX < this.x));
     }
   
    
     static preload(scene){
         scene.load.atlas('female', 'assets/images/female.png', 'assets/images/female_atlas.json');
         scene.load.animation('female_anim', 'assets/images/female_anim.json');
+        scene.load.spritesheet('items','assets/images/items.png',{frameWidth:32,frameHeight:32});
     }
 
     get velocity () {
@@ -46,6 +54,27 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         }else {
             this.anims.play('female_idle',true);
         }
-        
+        this.spriteWeapon.setPosition(this.x,this.y);
+        this.weaponRotate();
+    }
+    
+    weaponRotate(){
+        let pointer = this.scene.input.activePointer;
+        if(pointer.isDown){
+            this.weaponRotation += 6;   
+        }else{
+            this.weaponRotation = 0;
+        }
+        if(this.weaponRotation > 100){
+            this.weaponRotation = 0;
+        }
+        if(this.flipX){
+            this.spriteWeapon.setAngle(-this.weaponRotation -90);
+        }else{
+            this.spriteWeapon.setAngle(this.weaponRotation);
+        }
+
+
+    
     }
 }
